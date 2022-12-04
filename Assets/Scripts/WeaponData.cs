1189f49f986eaa29ee8ca8ca39707e5764e79b48
KID <50666207+KID421@users.noreply.github.com>
2022-12-04 15:58:52 +0800

    Update 武器資料

---
+++ Assets/Scripts/WeaponData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 武器資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "KID/Weapon Data", fileName = "New Weapon Data")]
+    public class WeaponData : ScriptableObject
+    {
+        [Header("武器攻擊力"), Range(0, 10000)]
+        public float attack;
+        [Header("武器物件數量、速度與位置")]
+        public WeaponObject[] weaponObjects;
+    }
+
+    [System.Serializable]
+    public class WeaponObject
+    {
+        [Header("武器速度")]
+        public Vector2 speed;
+        [Header("武器生成位置")]
+        public Vector3 pointSpawn;
+    }
+}
